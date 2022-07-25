1b9bbabcdf0a19fd93823edac11e13af4c28cbc0
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-25 14:49:41 -0300

    Controller: Iniciando Classe tarefaService

---
+++ private/controller/tarefaService.php
@@ -0,0 +1,24 @@
+<?php 
+// CRUD
+class TarefaService{
+    public function inserir()   // Create
+    {
+
+    }
+
+    public function recuperar()   // Read
+    {
+
+    }
+
+    public function atualizar()   // Update
+    {
+
+    }
+
+    public function remover() // delete
+    {
+        
+    }
+}
+?>  
