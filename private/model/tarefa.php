c1a3c8432d5dbecd7073a233633025555fdbf14d
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-25 14:41:47 -0300

    Criando a classe tarefa

---
+++ private/model/tarefa.php
@@ -0,0 +1,19 @@
+<?php 
+    class Tarefa{
+        private $id;
+        private $id_status;
+        private $tarefa;
+        private $data_cadastro;
+
+        public function __get($name)
+        {
+            return $this->$name;
+        }
+
+        public function __set($name, $value)
+        {
+            $this->$name = $value;
+        }
+        
+    }
+?>
