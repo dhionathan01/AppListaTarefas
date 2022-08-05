47db046d6f3ed20945d12f6513b8d2f72f49fd8e
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-08-05 14:23:01 -0300

    Alterando aplicações pra subir para produção

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	$acao = 'recuperarPendentes';
-	require 'controller/tarefa_controller.php';
+	require 'public/controller/tarefa_controller.php';
 ?>
 <html>
 	<head>
@@ -8,18 +8,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>App Lista Tarefas</title>
 
-		<link rel="stylesheet" href="css/estilo.css">
+		<link rel="stylesheet" href="public/css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-		<script src="js/editar_index.js"></script>
+		<script src="public/js/editar_index.js"></script>
 	</head>
 
 	<body>
 		<nav class="navbar navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="#">
-					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+					<img src="public/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
 					App Lista Tarefas
 				</a>
 			</div>
@@ -30,8 +30,8 @@
 				<div class="col-md-3 menu">
 					<ul class="list-group">
 						<li class="list-group-item active"><a href="#">Tarefas pendentes</a></li>
-						<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
-						<li class="list-group-item"><a href="todas_tarefas.php">Todas tarefas</a></li>
+						<li class="list-group-item"><a href="public/nova_tarefa.php">Nova tarefa</a></li>
+						<li class="list-group-item"><a href="public/todas_tarefas.php">Todas tarefas</a></li>
 					</ul>
 				</div>
 
